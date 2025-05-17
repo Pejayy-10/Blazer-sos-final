a61299c31625642982452e8cff1bb2654d7ea471
Pejay <frandilbertperuso@gmail.com>
2025-05-17 08:42:39 +0800

    Deployment Fix

--- docker-start.sh
+++ docker-start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Start Nginx
-service nginx start
+# Make sure storage and bootstrap/cache directories are writable
+chmod -R 777 /var/www/storage /var/www/bootstrap/cache
 
-# Start PHP-FPM
-php-fpm
+# Run heroku-php-nginx for Render deployment
+cd /var/www
+vendor/bin/heroku-php-nginx -C nginx.conf public/
